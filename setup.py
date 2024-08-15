6a800390f0b786d08e1b76121f82a0a03cc18486
Abdelrahman Yaghi <ayaghi@avertra.com>
2024-08-15 11:08:25 +0000

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name = 'internalTools',
-    version= '0.0.1',
-    packages=find_packages(),
-    install_requires=open('requirements.txt').readlines()
-)
+setup()
